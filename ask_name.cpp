5deb49b015d6edd4401573ec8a683ee29dd9e52c
Holy-Newton <deberdtfrancois@icloud.com>
2025-09-02 23:12:06 +0200

    starting lessons

---
+++ ask_name.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    cout<<"What is your name ?\n";
+    string name;
+    cin >> name;
+    cout<< "Hello World, and " << name << endl;
+    return 0;
+}
