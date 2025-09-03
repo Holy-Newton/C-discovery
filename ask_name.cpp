558a7881637acfff88dbd50532c692becc38c42f
Holy-Newton <deberdtfrancois@icloud.com>
2025-09-03 09:58:59 +0200

    test

--- ask_name.cpp
+++ ask_name.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
 #include<string>
-using namespace std;
+
 int main(){
-    cout<<"What is your name ?\n";
-    string name;
-    cin >> name;
-    cout<< "Hello World, and " << name << endl;
+    std::cout << "What is your name ?\n";
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello World, and " << name << std::endl;
+    std::cout << "Do you want to play a game ? (y/n)\n";
+    std::string response;
+    std::cin >> response;
+    if(response[0] == "y"){ //yes
+        std::cout << "ok let's play a game then " << name << " !!\n";
+    }
+    if(response == "n"){ //no
+        std::cout << "Ok dumbass..." ;
+    }
     return 0;
 }
