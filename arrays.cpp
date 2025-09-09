c8f8da39379440989fc8ea2742036515e29207f2
Holy-Newton <deberdtfrancois@icloud.com>
2025-09-09 21:00:00 +0200

    Commit on template< bla bla very interisting git add .

--- arrays.cpp
+++ arrays.cpp
@@ -3,23 +3,42 @@
 #include<string>
 #include<array>
 #include<vector>
+#include<deque>
+
+template<typename T> // some type we will c all T
+void print(T item, int size){
+    for(int i=0; i< size; i++){
+        std::cout<<item[i]<<std::endl;
+    }
+}
+
+template<typename T> // repeate after each call
+void print(T item){
+    for(auto i: item){
+        std::cout<<i<<std::endl;
+    }
+    }
+
 
 void print_string(std::string list1[], int size){
     for(int i=0 ; i < size ; i++){ 
+        list1[i] = "pie";
         std::cout<< list1[i] << std::endl;
     }
     std::cout << std::endl;
 }
-
 void printSTDarrays(std::array<std::string, 10> item){
     for(int i =0; i<item.size(); i++){
+        item[i] = "pie mhhhh";
         std::cout << item[i] << std::endl;
     }
     std::cout << std::endl;
 }       
 
 void printVector(std::vector<std::string> item){
+    std::cout << &item << std::endl;
     for(int i =0; i<item.size(); i++){
+        item[i] = "CAR";
         std::cout << item[i] << std::endl;
     }
     std::cout << std::endl;
@@ -43,11 +62,25 @@ int main(){
 
     std::cout << std::endl;
     print_string(food, 3); //BECAUSE STRING: not food.size() but sizeof(food)/sizeof(food[0])
+    std::cout << food << std::endl;
+    for(int i = 0; i<3;i++){
+        std::cout<<food[i]<<std::endl;
+    }
+    std::cout<<std::endl;
     printSTDarrays(more);
     printVector(brands);
     
-    std::cout << food << std::endl;
     std::cout << &more << std::endl;
     std::cout << &brands << std::endl;
+    for(auto i: brands){
+        std::cout<<i<<std::endl;
+    }
+
+
+    std::cout<<std::endl;
+    std::cout<<"==========new_tests=================================="<<std::endl;
+    print(food, 3);
+    print(more);
+    print(brands); // or + brands.size() for the size.
     return 0;
 }
