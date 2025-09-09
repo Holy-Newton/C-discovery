1c8e244552cbdace58a94599de6a18eb12b9641d
Holy-Newton <deberdtfrancois@icloud.com>
2025-09-09 22:12:27 +0200

    oriented prog

--- oriented_prog.cpp
+++ oriented_prog.cpp
@@ -8,9 +8,28 @@
 using namespace std;
 
 
+
+class Food {
+    public:
+        string name;
+        double cost;
+
+        void print(){
+            cout<<name<<"   "<<cost<<endl;
+        }
+};
+
 int main(){
 
-
+    Food food;
+    food.name = "bananas";
+    food.cost = 20.41;
+    Food food1;
+    food1.name = "coconuts";
+    food1.cost = 5.32;
+    //cout<< food.name <<"    "<<food.cost<<endl;
+    food.print();
+    food1.print();
     return 0;
 }
 
